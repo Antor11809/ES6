bb7df69fb9fd3572c8dd8017e393a76bc3f84264
Antor11809 <antormainuddin8@gmail.com>
2026-08-12 12:54:42 +0600

    abar

--- abarkorbam.js
+++ abarkorbam.js
@@ -1,33 +1,58 @@
-function generateLeaderboard(students) {
-    if (!Array.isArray == students) {
-        return "Invalid";
+let beststudent = (students)=> {
+    if(!Array.isArray(students)){
+        return "Array des nai to"
     }
-
-    if (students.length == 0) {
-        return "Invalid";
+    if(students.length == 0){
+        return "kisu to de"
     }
-for(let student of students){
-    if(!student.name ||  !"score" in students){
-        return "Invalid"
+    for(let student of students){
+        if(!student.name || !student.score){
+            return "number de score de"
+        }
+        if(typeof student.score !== "number"){
+            return "score cant be anything without number"
+        }
     }
-    if(typeof student.score !== "number"){
-        return "What have u done bruda"
-    }
+        let qualified = students.filter(function(student){
+            return student.score >= 70;
+        });
+        let name = qualified.map(function(student){
+            return student.name.toUpperCase()
+        });
+    
+    return name.slice(0, 3)
 }
-    const qualified = students.filter(student => {
-        return student.score > 70;
-    });
-
-    const names = qualified.map(({ name }) => {
-        return name.toUpperCase();
-    });
-    return names.slice(0, 4);
-}
-let m = [
+let nam = [
     { name: "Rafi", score: 90 },
-    { name: "Sadia", score: 656 },
+    { name: "Sadia", score: 65 },
     { name: "Karim", score: 85 },
-    { name: "Nafis", score: "75" },
-
+    { name: "Nafis", score: 75 },
+    {name: "frrf", score: 45}
 ]
-console.log(generateLeaderboard(m))
+console.log(beststudent(nam))
+
+
+
+let total = (scores)=>{
+if(!Array.isArray(scores)){
+    return "It is not an array"
+}
+if(scores.length == 0){
+    return "No length"
+}
+for (let score of scores){
+    if(typeof score !== "number"){
+        return "Number na dile jog korbo kemne"
+    }
+}
+    let studentsplus10 = scores.map(function(score){
+        return score + 10;
+    })
+    let add = studentsplus10.reduce(function(acc, current){
+        return acc + current;
+    }, 0)
+    return add;
+}
+let ed = [80, 65, 90, 75]
+
+console.log(total(ed))
